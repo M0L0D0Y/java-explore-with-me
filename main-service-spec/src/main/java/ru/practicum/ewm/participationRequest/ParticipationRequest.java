2d4fd15e651a0a9af24dbc1012e2e80c33a738f6
M0L0D0Y <i7ascha@gmail.com>
2022-10-28 15:15:54 +0300

    refactor: рефакторинг после 1го ревью. Удалил equals и hashCode по совету наставника(из второго модуля тоже)

--- main-service-spec/src/main/java/ru/practicum/ewm/participationRequest/ParticipationRequest.java
+++ main-service-spec/src/main/java/ru/practicum/ewm/participationRequest/ParticipationRequest.java
@@ -31,17 +31,4 @@ public class ParticipationRequest {
     private User requester;
     @Column(name = "status")
     private String status;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        ParticipationRequest that = (ParticipationRequest) o;
-        return id != null && Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
