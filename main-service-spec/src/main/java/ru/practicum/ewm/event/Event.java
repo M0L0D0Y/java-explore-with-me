d83bf753474d918563ec663c32599f9a8db8cb4d
M0L0D0Y <i7ascha@gmail.com>
2022-10-28 14:58:02 +0300

    refactor: рефакторинг после 1го ревью. Удалил equals и hashCode по совету наставника

--- main-service-spec/src/main/java/ru/practicum/ewm/event/Event.java
+++ main-service-spec/src/main/java/ru/practicum/ewm/event/Event.java
@@ -12,7 +12,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 @Setter
