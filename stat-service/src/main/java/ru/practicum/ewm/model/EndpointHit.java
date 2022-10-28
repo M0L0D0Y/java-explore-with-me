5b88bfb8009dd01c162af7a6710100931f8c5855
M0L0D0Y <i7ascha@gmail.com>
2022-10-28 15:14:18 +0300

    refactor: рефакторинг после 1го ревью. Удалил equals и hashCode по совету наставника(из второго модуля тоже)

--- stat-service/src/main/java/ru/practicum/ewm/model/EndpointHit.java
+++ stat-service/src/main/java/ru/practicum/ewm/model/EndpointHit.java
@@ -4,13 +4,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -34,17 +32,4 @@ public class EndpointHit {
     @NotNull(message = "Не указан timestamp обновляемого события")
     @Column(name = "timestamp")
     private LocalDateTime timestamp;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        EndpointHit that = (EndpointHit) o;
-        return id != null && Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
